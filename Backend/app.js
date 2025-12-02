5ca924539e84cb30d8653567b4a2309c6d5a3879
Gyanendra876 <harshsinha06z@gmail.com>
2025-12-02 20:34:22 +0530

    Update FRONTEND_URL assignment in app.js
    Removed default value for FRONTEND_URL.

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+const FRONTEND_URL = process.env.FRONTEND_URL ;
 app.use(cors({
   origin: [FRONTEND_URL],
   credentials: true
